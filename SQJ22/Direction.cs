dc6b8196e8ed89d27217d36cc6034c47633a4bfe
notexplosive <thenotexplosive@gmail.com>
2022-11-19 13:04:04 -0800

    add direction class

---
+++ SQJ22/Direction.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace SQJ22;
+
+public record Direction
+{
+    public static readonly Direction Down = new(new Point(0, 1));
+    public static readonly Direction Up = new(new Point(0, -1));
+    public static readonly Direction Left = new(new Point(-1, 0));
+    public static readonly Direction Right = new(new Point(1, 0));
+    public static readonly Direction None = new(new Point(0, 0));
+
+    private Direction(Point asPoint)
+    {
+        AsPoint = asPoint;
+    }
+
+    public Point AsPoint { get; }
+
+    public Direction NextCounterClockwise()
+    {
+        if (this == Direction.Up)
+        {
+            return Direction.Left;
+        }
+
+        if (this == Direction.Right)
+        {
+            return Direction.Up;
+        }
+
+        if (this == Direction.Down)
+        {
+            return Direction.Right;
+        }
+
+        if (this == Direction.Left)
+        {
+            return Direction.Down;
+        }
+
+        return Direction.None;
+    }
+
+    public Direction NextClockwise()
+    {
+        if (this == Direction.Up)
+        {
+            return Direction.Right;
+        }
+
+        if (this == Direction.Right)
+        {
+            return Direction.Down;
+        }
+
+        if (this == Direction.Down)
+        {
+            return Direction.Left;
+        }
+
+        if (this == Direction.Left)
+        {
+            return Direction.Up;
+        }
+
+        return Direction.None;
+    }
+
+    public Direction Next(Rotation rotation)
+    {
+        return rotation switch
+        {
+            Rotation.Clockwise => NextClockwise(),
+            Rotation.CounterClockwise => NextCounterClockwise(),
+            _ => Direction.None
+        };
+    }
+}
