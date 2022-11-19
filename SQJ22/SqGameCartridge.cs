1bb5d0379c2732a2822d706f4d28ae9d19119439
notexplosive <thenotexplosive@gmail.com>
2022-11-18 17:15:08 -0800

    initial commit

---
+++ SQJ22/SqGameCartridge.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ExplogineCore;
+using ExplogineMonoGame;
+using ExplogineMonoGame.Cartridges;
+using Microsoft.Xna.Framework;
+
+namespace SQJ22;
+
+public class SqGameCartridge : BasicGameCartridge
+{
+    public override CartridgeConfig CartridgeConfig { get; } = new(new Point(1920, 1080));
+    public override void AddCommandLineParameters(CommandLineParametersWriter parameters)
+    {
+    }
+
+    public override IEnumerable<ILoadEvent> LoadEvents(Painter painter)
+    {
+        yield return null;
+    }
+
+    public override void OnCartridgeStarted()
+    {
+    }
+
+    public override void Update(float dt)
+    {
+    }
+
+    public override void Draw(Painter painter)
+    {
+    }
+}
